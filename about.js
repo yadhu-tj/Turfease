51c7c934d7bfe42514139d5724a59e1c18066c36
Yadhu <yadhunandhantj@gmail.com>
2026-01-04 23:56:54 +0530

    all other files are changed after structuring

--- about.js
+++
@@ -1,4 +0,0 @@
-// about.js
-document.getElementById('learnMoreBtn').addEventListener('click', function() {
-    window.location.href = 'more-info.html';  // Replace with the appropriate link for more details
-});
